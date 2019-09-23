874c4f1b9c153234163d0b25c32c908ceb17435b
Nick Stone <njstone1983@gmail.com>
2019-09-22 23:52:23 -0500

    Added DLC characters
    Added two new characters, fucked around with some styling and added a character bio

--- meats-of-rage/main.js
+++ meats-of-rage/main.js
@@ -8,26 +8,49 @@
     'url("../images/ainsley-bg.jpeg")',
     'url("../images/marco-bg.jpg")',
     'url("../images/gordon-bg.jpg")',
-    'url("../images/keith-bg.jpeg")'
+    'url("../images/keith-bg.jpeg")',
+    'url("../images/guy-bg.jpg")',
+    'url("../images/anthony-bg.jpg")'
   ];
 
   let imagesToCache = [
     "../images/ainsley-bg.jpeg",
     "../images/ainsley.png",
+    "../images/anthony.png",
+    "../images/guy.png",
     "../images/beef.jpg",
     "../images/gordon-bg.jpg",
     "../images/gordon.png",
     "../images/marco-bg.jpg",
     "../images/marco.png",
     "../images/keith-bg.jpeg",
+    "../images/anthony-bg.jpg",
+    "../images/guy-bg.jpg",
+  ];
+
+  let summaries = [
+    "Ainsley is a formidable close-range fighter, deploying his MEAT RUB and SPECIAL AROMA to vanquish any and all foes",
+    "Marco is the best ranged fighter, launching STOCK POTS and STOCK CUBES great distances.",
+    "Gordon is a well-rounded fighter, deploying the IDIOT SANDWICH at close range and general VERBAL ABUSE over distance.",
+    "Keith has a powerful, but inaccurate ranged WINE BOTTLE attack, and his perma-pissed state allows him to absorb damage.",
+    "Guy is pretty useless and basically chucks BURGERS at people, but his natural armour allows him to take a lot of damage.",
+    "Anthony is the weakest fighter, and his ANNOYING LITTLE CUNT attack is limited, but his tiny stature means that many attacks will go wide of their mark."
   ];
 
   let charSelectBox       = document.querySelector("#char-select-box");
   let charSelectScreen    = document.querySelector("#char-select-screen");
   let meatYourFate        = document.querySelector("#meat-your-fate-button");
-
   let preloadScreen       = document.querySelector("#preload-screen");
   let loadingProgressText = document.querySelector("#loading-progress-text");
+  let mainScreen          = document.querySelector("#main-screen");
+  let canvas              = document.querySelector("#canvas");
+  let summary             = document.querySelector("#character-summary");
+
+  let game = new Game({
+    container : mainScreen,
+    canvas    : canvas
+  });
+
 
   let States = {
     CharSelect : 0
@@ -40,7 +63,27 @@
       case States.CharSelect:
         setStateCharSelect();
         break;
+      case States.GameIntro:
+        unsetStateCharSelect();
+        setStateGameIntro();
+        break;
     }
+  }
+
+  function setStateGameIntro()
+  {
+
+  }
+
+  function unsetStateCharSelect()
+  {
+    let boxStyle = charSelectBox.style;
+    boxStyle.transform = "scale(0)";
+    boxStyle.opacity = 0;
+    charSelectScreen.style.opacity = 0;
+    setTimeout(()=>{
+      charSelectScreen.style.display = "none";
+    }, 1000);
   }
 
   function setStateCharSelect()
@@ -67,6 +110,7 @@
       meatYourFate.style.visibility = "visible";
       meatYourFate.style.transform = "scale(1)";
       selectedCharIndex = i;
+      summary.innerHTML = summaries[i];
       charSelectScreen.style.backgroundImage = bgImages[i];
       console.log("cccc", charSelectScreen.style);
       drawCharacterSelection();
@@ -106,6 +150,9 @@
   }
 
 
+  meatYourFate.addEventListener("click", ()=>{
+    setState(States.GameIntro);
+  });
 
 
   loadImages(imagesToCache,
